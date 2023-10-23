6a057f59e48385cd6473871864e80e18dcdfa07e
Devansh Thakkar <devansh@Devanshs-MacBook-Pro.local>
2023-10-23 14:34:20 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+  },
+  extend: {
+    colors: {
+      clifford: '#da373d',
+      twitterBlue: '#1D9BF0',
+      twitterblueHover: '#1871CA',
+      twitterbluedis: '#1E5D87',
+      neutral1000: '#000000',
+      neutral50: '#F9F9F9',
+      neutral200: '#e4e4e4',
+      neutral700: '#404040'
+    }
+  },
+  plugins: [],
+}
+
